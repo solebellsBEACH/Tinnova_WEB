c1f87c7989034e9addc89312e3ac17c0a84a70ba
solebellsBEACH <lucassxxavier@gmail.com>
2022-09-24 09:57:06 -0300

    fazendo adequações na estrutura

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+// import { store } from "../core/store";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <ChakraProvider>
+    {/* <Provider store={store}> */}
+    <Component {...pageProps} />
+    {/* </Provider> */}
+  </ChakraProvider>
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+// import HomeComponent from './Home'
+
+const Index: NextPage = () => {
+  return (
+    <div>Página Home</div>
+    // <HomeComponent />
+  )
+}
+
+export default Index
